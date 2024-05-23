0c6e067f41dc63b345bf6bff42ba211f3fdc7d07
maniziva-manikandan <adaikalam.manikandan@moneyforward.co.jp>
2024-05-23 15:49:33 +0530

    testrail update7

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   // workers: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['allure-playwright'],['junit']],
+  reporter: [['html'], ['allure-playwright'],['junit', { outputFile: 'test-results/junit-report.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
