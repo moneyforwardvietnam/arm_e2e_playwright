a2675fd97d5d39f0ed8d984523fe5e616f5e15e4
maniziva-manikandan <adaikalam.manikandan@moneyforward.co.jp>
2024-05-27 13:44:24 +0530

    C165025 - 1st case commit

--- RegressionTests/TransactionsMenu/Opportunity/ARM-6094/ARM-6094_TestSuite.spec.ts
+++ RegressionTests/TransactionsMenu/Opportunity/ARM-6094/ARM-6094_TestSuite.spec.ts
@@ -3,14 +3,28 @@ import { Default_claim_deposit_accounts_Tax } from '@pages/Settings/Default_clai
 import { OpportunityPage } from '@pages/TransactionsMenu/Opportunity/OpportunityPages.page'
 
 test.describe('ARM-6094_TestSuite', async () => {
-  test('OfficeSettingMessage', async ({ page }) => {
+  test('C165025_OfficeTaxSettingTooltip', async ({ page }) => {
     const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
     await TaxSettingPage.NavigateToOfficeTaxSettingScreen();
     await page.locator('section').getByRole('img').click(); //? Question mark icon
     await expect(page.locator("//div[@role='tooltip']")).toHaveText('請求書にて、品目の単価を税込（内税）もしくは税抜（外税）として計算します。');
   });
 
-  test('SetTaxExcludeSetting', async ({ page }) => {
+  test('C165026_TaxExcludeSetting_OpportunityCreateScreen', async ({ page }) => {
+    const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
+    const opportunityPage = new OpportunityPage(page);
+    //Set Tax setting as Tax Exclude
+    await TaxSettingPage.NavigateToOfficeTaxSettingScreen();
+    await TaxSettingPage.SetTaxExclude();
+    // Navigate to Opportunity
+    await opportunityPage.NavigateToOpportunity();
+    await opportunityPage.NewOpportunity();
+    await expect(opportunityPage.Cell.nth(1)).toHaveText('単価 税抜');
+    await expect(opportunityPage.Cell.nth(3)).toHaveText('販売合計額 税抜');
+    await expect(opportunityPage.Cell.nth(5)).toHaveText('販売合計額 税込');
+  });
+
+  test('C165027_TaxExcludeSetting_OpportunityDetailScreen', async ({ page }) => {
     const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
     const opportunityPage = new OpportunityPage(page);
     //Set Tax setting as Tax Exclude
@@ -20,7 +34,35 @@ test.describe('ARM-6094_TestSuite', async () => {
     await opportunityPage.NavigateToOpportunity();
     await opportunityPage.NewOpportunity();
   });
-  test('SetTaxInludeSetting', async ({ page }) => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  test('C165033_TaxIncludeSetting_OpportunityCreateScreen', async ({ page }) => {
+    const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
+    const opportunityPage = new OpportunityPage(page);
+    //Set Tax setting as Tax Include
+    await TaxSettingPage.NavigateToOfficeTaxSettingScreen();
+    await TaxSettingPage.SetTaxInclude();
+    // Navigate to Opportunity
+    await opportunityPage.NavigateToOpportunity();
+    await opportunityPage.NewOpportunity();
+    await expect(opportunityPage.Cell.nth(1)).toHaveText('単価 税込');
+    await expect(opportunityPage.Cell.nth(3)).toHaveText('販売合計額 税込');
+  });
+
+  test('C165034_TaxIncludeSetting_OpportunityCreateScreenHideColumn', async ({ page }) => {
     const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
     const opportunityPage = new OpportunityPage(page);
     //Set Tax setting as Tax Include
@@ -30,5 +72,26 @@ test.describe('ARM-6094_TestSuite', async () => {
     await opportunityPage.NavigateToOpportunity();
     await opportunityPage.NewOpportunity();
   });
+  
+  test('C165035_TaxIncludeSetting_OpportunityDetailScreen', async ({ page }) => {
+    const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
+    const opportunityPage = new OpportunityPage(page);
+    //Set Tax setting as Tax Include
+    await TaxSettingPage.NavigateToOfficeTaxSettingScreen();
+    await TaxSettingPage.SetTaxInclude();
+    // Navigate to Opportunity
+    await opportunityPage.NavigateToOpportunity();
+    await opportunityPage.NewOpportunity();
+  });
+  test('C165036_TaxIncludeSetting_OpportunityDetailScreenHideColumn', async ({ page }) => {
+    const TaxSettingPage = new Default_claim_deposit_accounts_Tax(page);
+    const opportunityPage = new OpportunityPage(page);
+    //Set Tax setting as Tax Include
+    await TaxSettingPage.NavigateToOfficeTaxSettingScreen();
+    await TaxSettingPage.SetTaxInclude();
+    // Navigate to Opportunity
+    await opportunityPage.NavigateToOpportunity();
+    await opportunityPage.NewOpportunity();
+  });   
 });
 
