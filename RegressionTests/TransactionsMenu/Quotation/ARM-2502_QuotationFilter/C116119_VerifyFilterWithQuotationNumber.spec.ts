b625d396f086ffe240bbf37f29ffaa1d299a1548
JohaanShakir <alam.shakir@moneyforward.co.jp>
2024-06-07 10:23:48 +0530

    Create Quotation
    Create Quotation

--- RegressionTests/TransactionsMenu/Quotation/ARM-2502_QuotationFilter/C116119_VerifyFilterWithQuotationNumber.spec.ts
+++ RegressionTests/TransactionsMenu/Quotation/ARM-2502_QuotationFilter/C116119_VerifyFilterWithQuotationNumber.spec.ts
@@ -2,12 +2,13 @@ import { test, expect } from '@playwright/test';
 import { QuotationPage } from '@pages/TransactionsMenu/Quotation/QuotationPages.page';
 
 test('C116119_VerifyFilterWithQuotationNumber.spec', async ({ page }) => {
-//Quotation Creation
+//Create Quotation
 const quotationPage = new QuotationPage(page)
 await quotationPage.NavigateToQuotation();
 await quotationPage.QuotationNumFilter.click();
 await quotationPage.QuotationNumFilter.fill('111');
 await quotationPage.QuotationFilterBtn.click();
 await expect(quotationPage.FirstRowQuotation).toHaveText('111');
+
 
 });
